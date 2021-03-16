6da1e298107342a78d46cdf4f7e80c5675a0349e
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-16 20:25:21 +0000

    proj initialized with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: 'Montserrat', 
+        Poppins: 'Poppins', 
+        Ubuntu: 'Ubuntu', 
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
